2cd96303f20be3a20e134159f5f3b9ce1513d2ac
roboticsysinfo <roboticsysinfo@gmail.com>
2025-07-21 11:53:52 +0530

    featured category route

--- routes/quoteCategoryRoutes.js
+++ routes/quoteCategoryRoutes.js
@@ -5,7 +5,8 @@ const {
   getCategoryById,
   createCategory,
   updateCategory,
-  deleteCategory
+  deleteCategory,
+  getFeaturedCategories
 } = require('../controllers/quoteCategoryController');
 const { adminOnly, protect } = require('../middleware/authMiddleware');
 
@@ -23,5 +24,8 @@ router.put('/update/category/:id', protect, adminOnly, updateCategory);
 
 // DELETE
 router.delete('/delete/category/:id', protect, adminOnly, deleteCategory);
+
+// GET one
+router.get('/featured-categories', getFeaturedCategories);
 
 module.exports = router;
